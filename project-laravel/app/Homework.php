61ee91818d58bf5b28f7b4b3c874eb8958daf74d
lambdadigamma <lennart.fischer@adolfinum.de>
2017-09-09 15:20:43 +0200

    Updated models

--- project-laravel/app/Homework.php
+++ project-laravel/app/Homework.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Homework extends Model
 {
-    //
+
+    public function course() {
+
+        $this->belongsTo('App\Course');
+
+    }
+
+    public function users() {
+
+        $this->belongsToMany('App\User');
+
+    }
+
 }
