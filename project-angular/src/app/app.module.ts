671bb2c81be8eaf94fe98f06d2301bb4d904375f
Maximilian Schorradt <schorradt@publicplan.de>
2017-09-09 13:06:23 +0200

    Add userService Add components Add user model

--- project-angular/src/app/app.module.ts
+++ project-angular/src/app/app.module.ts
@@ -2,13 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { RouterModule, Routes } from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { IndexPageComponent } from './index-page/index-page.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'index',
+    component: IndexPageComponent,
+    data: { title: 'Index' }
+  },
+  { path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    PageNotFoundComponent,
+    IndexPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes, {
+        enableTracing: true
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ project-angular/src/app/user.ts
@@ -0,0 +1,6 @@
+export class User {
+  private id: Number;
+  private name: String;
+  private email: String;
+  public roles: Array;
+}
