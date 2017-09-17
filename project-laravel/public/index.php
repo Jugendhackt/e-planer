33d378f4176c78e85851985317b0d7d89dbd8b6f
labcode-de <leo@labcode.de>
2017-09-17 13:52:50 +0200

    Change homework btn-success to btn-dark-green, changed CORS Middleware to new Angular Docker Port

--- project-laravel/public/index.php
+++ project-laravel/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: http://localhost:4200');
+header('Access-Control-Allow-Origin: http://localhost:8082');
 header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 header('Access-Control-Allow-Credentials: true');
